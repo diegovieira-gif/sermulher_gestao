0a7841ab4fb03df83444786cbebb085ffc4a363c
Diego Vieira <diegovenanciovieira@gmail.com>
2026-03-26 15:11:56 -0300

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -3,10 +3,24 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   output: "standalone",
 
+  // Otimização do Next.js 16
   reactCompiler: true,
 
   typescript: {
     ignoreBuildErrors: true,
+  },
+
+  // 👇 CORREÇÃO: Liberação do proxy Traefik para as Server Actions 👇
+  experimental: {
+    serverActions: {
+      allowedOrigins: [
+        "fluxo-sermulher.aracaju.se.gov.br",
+        "*.aracaju.se.gov.br",
+        "localhost:3000",
+        "localhost:3002",
+        "192.168.0.115:3002"
+      ],
+    },
   },
 
   images: {
